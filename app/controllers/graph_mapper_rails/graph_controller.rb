672e606b1ce6785c19aee2997a9030af1da003c8
parroty <parroty00@gmail.com>
2012-06-24 19:25:00 +0900

    added mulitple graph support

--- app/controllers/graph_mapper_rails/graph_controller.rb
+++ app/controllers/graph_mapper_rails/graph_controller.rb
@@ -6,10 +6,33 @@ require 'graph_adapter/highchart'
 module GraphMapperRails
   class GraphController < ApplicationController
     def index
-      m = Initializer.mapper
-      h = GraphAdapter::Highchart.new({:title => "Monthly Graph"})
-      h.data({:name => "Graph", :key => m.keys, :value => m.values, :color => '#4572A7'})
-      @chart = h.get_charts
+      @charts = []
+      config = Initializer.config
+      config.klass.graph_keywords.each do | keyword |
+        m = config.mapper(keyword)
+        h = GraphAdapter::Highchart.new({:title => keyword})
+
+        chart = h.get_charts
+
+        chart.chart(:defaultSeriesType => "line", :height => 250, :borderRadius => 1)
+        chart.xAxis(:categories => m.keys, :tickInterval => 2)
+        chart.series(:name => keyword, :yAxis => 0, :data => m.values, :animation => false, :color => config.colors[:line])
+
+        if config.use_average
+          chart.series(:name => "average", :yAxis => 0, :data => [[0, m.average],[m.keys.size - 1, m.average]],
+                       :animation => false, :color => config.colors[:average])
+        end
+
+        if config.moving_average_length
+          graph_name = "moving average for #{config.moving_average_length} items"
+          chart.series(:name => graph_name, :yAxis => 0, :data => m.moving_average.map,
+                       :animation => false, :dashStyle => 'ShortDash', :color => config.colors[:moving_average])
+        end
+
+        @charts << h.get_charts
+      end
+
+      @charts
     end
   end
 end
--- lib/graph_mapper_rails.rb
+++ lib/graph_mapper_rails.rb
@@ -3,35 +3,41 @@ require "graph_mapper_rails/engine"
 module GraphMapperRails
   class Initializer
     def self.setup
-      yield self
+      @@config = Config.new
+      yield @@config
     end
 
-    def self.set_class(klass)
-      @@klass ||= klass
+    def self.config
+      @@config
     end
-    
-    def self.set_options(&block)
+  end
+
+  class Config
+    attr_accessor :klass, :duration, :use_average, :colors
+
+    def set_options(&block)
       option_mapper = OptionMapper.new
       yield option_mapper
-      @@options = option_mapper.hash
-    end
-    
-    def self.set_duration(duration)
-      @@duration = duration
-    end
-    
-    def self.set_mapper(&block)
-      @@block = block
+      @options = option_mapper.hash
     end
 
-    def self.mapper
-      manager = @@klass.all
+    def set_mapper(&block)
+      @block = block
+    end
 
-      record_mapper = RecordMapper.new
-      GraphMapper::Mapper.new(manager, DateTime.now - @@duration, DateTime.now, @@options) do | record |
-        @@block.call(record_mapper, record)
+    def mapper(keyword = nil)
+      manager = @klass.all
+
+      GraphMapper::Mapper.new(manager, DateTime.now - @duration, DateTime.now, @options) do | record |
+        record_mapper = RecordMapper.new
+        record_mapper.keyword = keyword
+        @block.call(record_mapper, record)
         record_mapper.hash
       end
+    end
+
+    def moving_average_length
+      @options[:moving_average_length]
     end
   end
 
@@ -39,34 +45,31 @@ module GraphMapperRails
     def initialize
       @hash = Hash.new
     end
-    
+
     def span_type=(span_type)
       @hash[:span_type] = span_type
     end
-    
+
     def date_format=(date_format)
       @hash[:date_format] = date_format
     end
-    
+
+    def moving_average_length=(length)
+      @hash[:moving_average_length] = length
+    end
+
     def hash
       @hash
     end
   end
-  
-  class RecordMapper
-    def initialize
-      @hash = Hash.new
-    end
 
-    def key=(key)
-      @hash[:key] = key
-    end
-    
-    def num=(num)
-      @hash[:value] = num
-    end
+  class RecordMapper
+    attr_accessor :keyword, :key, :num
 
     def hash
+      @hash = Hash.new
+      @hash[:key]   = @key
+      @hash[:value] = @num || 0
       @hash
     end
   end
