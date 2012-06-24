596844d5da63ddb98b70959a01c532b8158e34e9
parroty <parroty00@gmail.com>
2012-06-24 19:29:41 +0900

    refactored variable names

--- app/controllers/graph_mapper_rails/graph_controller.rb
+++ app/controllers/graph_mapper_rails/graph_controller.rb
@@ -6,13 +6,13 @@ require 'graph_adapter/highchart'
 module GraphMapperRails
   class GraphController < ApplicationController
     def index
-      @charts = []
       config = Initializer.config
-      config.klass.graph_keywords.each do | keyword |
-        m = config.mapper(keyword)
-        h = GraphAdapter::Highchart.new({:title => keyword})
 
-        chart = h.get_charts
+      @charts = []
+      @klass_name = config.klass
+      @klass_name.graph_keywords.each do | keyword |
+        m = config.mapper(keyword)
+        chart = GraphAdapter::Highchart.new({:title => keyword}).get_charts
 
         chart.chart(:defaultSeriesType => "line", :height => 250, :borderRadius => 1)
         chart.xAxis(:categories => m.keys, :tickInterval => 2)
@@ -29,7 +29,7 @@ module GraphMapperRails
                        :animation => false, :dashStyle => 'ShortDash', :color => config.colors[:moving_average])
         end
 
-        @charts << h.get_charts
+        @charts << chart
       end
 
       @charts
