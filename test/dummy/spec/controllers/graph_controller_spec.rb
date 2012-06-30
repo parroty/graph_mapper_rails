547736260d3e2c31e240e4acd9fbc316d964d38e
parroty <parroty00@gmail.com>
2012-06-30 11:50:03 +0900

    controller spec modified

--- test/dummy/spec/controllers/graph_controller_spec.rb
+++ test/dummy/spec/controllers/graph_controller_spec.rb
@@ -6,6 +6,17 @@ module GraphMapperRails
 describe GraphController do
   describe "show pages" do
     before(:each) { @routes = GraphMapperRails::Engine.routes }
+    before do
+      class << Sample
+        def from_graph_mapper_keywords
+          ["aaa", "title", "hoge"]
+        end
+
+        def from_graph_mapper_series(keyword, length)
+          {:name => "estimate", :data => [[0, 3],[length - 1, 3]], :color => "green"}
+        end
+      end
+    end
 
     it "should show index page" do
       get :index
@@ -13,6 +24,29 @@ describe GraphController do
       response.should be_success
       assigns(:charts).size.should == Sample.from_graph_mapper_keywords.size
     end
+
+    it "should show index page - no from_graph_mapper_keywords" do
+      class << Sample
+        undef_method :from_graph_mapper_keywords
+      end
+
+      get :index
+
+      response.should be_success
+      assigns(:charts).size.should == 0
+    end
+
+    it "should show index page - no from_graph_mapper_series" do
+      class << Sample
+        undef_method :from_graph_mapper_series
+      end
+
+      get :index
+
+      response.should be_success
+      assigns(:charts).size.should == Sample.from_graph_mapper_keywords.size
+    end
+
   end
 end
 
