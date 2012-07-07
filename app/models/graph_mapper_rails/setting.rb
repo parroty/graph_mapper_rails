520ebf5fd580dd2700ae5990d68fae4c79986772
parroty <parroty00@gmail.com>
2012-07-07 10:00:51 +0900

    updated Setting class

--- app/models/graph_mapper_rails/setting.rb
+++ app/models/graph_mapper_rails/setting.rb
@@ -2,14 +2,23 @@ module GraphMapperRails
   class Setting < ActiveRecord::Base
     attr_accessible :key, :value, :tags_attribute
 
-    def self.get_option(klass, key)
-      Setting.find(:first, :conditions => ["key = ?", [klass.to_s, key].join(".")]).try(:value)
+    def self.get_option(key)
+      find_by_key(key).try(:value)
     end
 
-    def self.set_option(klass, key, value)
-      record = Setting.find_or_create_by_key([klass.to_s, key].join("."))
+    def self.set_option(key, value)
+      record = Setting.find_or_create_by_key(key)
       record.value = value
       record.save
+    end
+
+    def self.remove_option(key)
+      find_by_key(key).try(:destroy)
+    end
+
+  private
+    def self.find_by_key(key)
+      Setting.where(["key = ?", key]).first
     end
   end
 end
