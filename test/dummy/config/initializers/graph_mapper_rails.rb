47a18dfb7ec72306be39d5fd4de85dfadb32b3f2
parroty <parroty00@gmail.com>
2012-07-15 01:12:23 +0900

    removed config.duration from initializer

--- test/dummy/config/initializers/graph_mapper_rails.rb
+++ test/dummy/config/initializers/graph_mapper_rails.rb
@@ -2,7 +2,6 @@ GraphMapperRails::Initializer.setup do |config|
   config.mapper_klass = Sample
 
   config.use_average = true
-  config.duration = 3.months
 
   config.colors = {
     :line => 'red',
